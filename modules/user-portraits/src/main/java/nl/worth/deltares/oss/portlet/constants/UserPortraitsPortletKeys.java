fba8d8ab6357dfadfe8f0cd75fe27346e3e1b9da
Pier Gaetani <pgaetani@worth.systems>
2019-06-19 09:58:29 +0200

    Cleaned up pom.xml files (using only gradle now), upgraded user-portraits portlet

---
+++ modules/user-portraits/src/main/java/nl/worth/deltares/oss/portlet/constants/UserPortraitsPortletKeys.java
@@ -0,0 +1,10 @@
+package nl.worth.deltares.oss.portlet.constants;
+
+
+/**
+ * @author Pier-Angelo Gaetani @ Worth Systems
+ */
+public class UserPortraitsPortletKeys {
+
+	public static final String USER_PORTRAITS = "userportraits";
+}
