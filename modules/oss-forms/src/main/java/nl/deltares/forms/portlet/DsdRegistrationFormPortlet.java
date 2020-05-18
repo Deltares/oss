73c71237abfe3f4b4e6b2afd3c5fb52339fa20e7
Erik de Rooij <erik.derooij@deltares.nl>
2020-05-18 15:00:57 +0200

    Portlets added to Fragments are not fully rendered

--- modules/oss-forms/src/main/java/nl/deltares/forms/portlet/DsdRegistrationFormPortlet.java
+++ modules/oss-forms/src/main/java/nl/deltares/forms/portlet/DsdRegistrationFormPortlet.java
@@ -27,6 +27,7 @@ import java.util.Map;
 @Component(
 	immediate = true,
 	property = {
+        "com.liferay.fragment.entry.processor.portlet.alias=registration-buttons",
 		"com.liferay.portlet.display-category=OSS",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
