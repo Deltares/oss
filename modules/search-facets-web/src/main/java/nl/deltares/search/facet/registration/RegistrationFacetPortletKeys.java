9431c02449b77144ff9e136529f0dc5da3ec588c
Allan Revolorio <allan@catalystdevs.com>
2020-06-03 08:45:58 +0200

    Add registration search facet
    This facet restricts the results in the search portlet and only shows
    content of type: registration web content structure

---
+++ modules/search-facets-web/src/main/java/nl/deltares/search/facet/registration/RegistrationFacetPortletKeys.java
@@ -0,0 +1,11 @@
+package nl.deltares.search.facet.registration;
+
+/**
+ * @author allan
+ */
+public class RegistrationFacetPortletKeys {
+
+	public static final String REGISTRATION_FACET_PORTLET =
+		"nl_deltares_search_facet_registration_RegistrationFacetPortlet";
+
+}
