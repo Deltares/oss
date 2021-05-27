d4b482d8d22819462fb015d1a39357b5d6b2fc42
rooij_e <erik.derooij@deltares.nl>
2021-05-27 15:29:33 +0200

    MYDELTARES-184: Make search facets TOPIC and TYPE more genenric

--- modules/search-facets-web/src/main/java/nl/deltares/search/constans/FacetPortletKeys.java
+++ modules/search-facets-web/src/main/java/nl/deltares/search/constans/FacetPortletKeys.java
@@ -17,12 +17,6 @@ public class FacetPortletKeys {
     public static final String DATE_RANGE_FACET_PORTLET =
             "nl_deltares_search_facet_date_DateRangeFacetPortlet";
 
-    public static final String SESSION_TYPE_FACET_PORTLET =
-            "nl_deltares_search_facet_type_SessionTypeFacetPortlet";
-
-    public static final String SESSION_TOPIC_FACET_PORTLET =
-            "nl_deltares_search_facet_topic_SessionTopicFacetPortlet";
-
     public static final String EVENT_FACET_PORTLET =
             "nl_deltares_search_facet_event_EventFacetPortlet";
 
