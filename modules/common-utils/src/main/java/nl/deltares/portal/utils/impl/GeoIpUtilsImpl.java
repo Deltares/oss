c8827208b72dd7beca876511c673ece36083c4ab
rooij_e <erik.derooij@deltares.nl>
2021-05-31 11:43:22 +0200

    MYDELTARES-217: DSD: pre select country in registration form

---
+++ modules/common-utils/src/main/java/nl/deltares/portal/utils/impl/GeoIpUtilsImpl.java
@@ -0,0 +1,60 @@
+package nl.deltares.portal.utils.impl;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import nl.deltares.portal.utils.GeoIpUtils;
+import org.osgi.service.component.annotations.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component(
+        immediate = true,
+        service = GeoIpUtils.class
+)
+public class GeoIpUtilsImpl implements GeoIpUtils {
+
+    private static final Log LOG = LogFactoryUtil.getLog(GeoIpUtilsImpl.class);
+
+    public final static String PROP_GEOIP_DB_DIR = "maxmind.geoip.database.dir";
+    public final static String PROP_GEOIP_DB_NAME = "maxmind.geoip.database.name";
+
+    private final DatabaseReader reader;
+
+
+    public GeoIpUtilsImpl() throws IOException {
+
+        String dbDir = PropsUtil.get(PROP_GEOIP_DB_DIR);
+        String dbName = PropsUtil.get(PROP_GEOIP_DB_NAME);
+
+        File database = new File(dbDir + "/" + dbName);
+        reader = new DatabaseReader.Builder(database).build();
+
+    }
+
+    @Override
+    public Map<String, Object> getLocationInfo(String ipAddress){
+        HashMap<String, Object> info = new HashMap<>();
+        try {
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+            CityResponse city = reader.city(inetAddress);
+            info.put("city", city.getCity().getName());
+            info.put("postal", city.getPostal().getCode());
+            info.put("country", city.getCountry().getName());
+            info.put("iso_code", city.getCountry().getIsoCode());
+            info.put("latitude", city.getLocation().getLatitude());
+            info.put("longitude", city.getLocation().getLongitude());
+        } catch (GeoIp2Exception | IOException e) {
+            LOG.warn("Error creating location info response: " + e.getMessage());
+        }
+        return info;
+    }
+
+}
---
+++ modules/common-utils/src/test/java/nl/deltares/portal/utils/impl/GeoIpUtilsImplTest.java
@@ -0,0 +1,43 @@
+package nl.deltares.portal.utils.impl;
+
+import com.liferay.portal.kernel.util.PropsUtil;
+import nl.deltares.mock.MockProps;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+public class GeoIpUtilsImplTest {
+
+    private GeoIpUtilsImpl geoIpUtils;
+
+    @Before
+    public void setup() throws IOException {
+
+        MockProps props = new MockProps();
+        Properties properties = new Properties();
+        properties.put(GeoIpUtilsImpl.PROP_GEOIP_DB_DIR, getClass().getResource("/geoip/").getFile());
+        properties.put(GeoIpUtilsImpl.PROP_GEOIP_DB_NAME, "GeoLite2-City.mmdb");
+        props.setProperties(properties);
+        PropsUtil.setProps(props);
+        geoIpUtils = new GeoIpUtilsImpl();
+
+    }
+
+    @Test
+    public void testLocationInfo() {
+
+        String ipAddress = "77.166.13.240";
+
+        Map<String, Object> locationInfo = geoIpUtils.getLocationInfo(ipAddress);
+        Assert.assertEquals("Delft", locationInfo.get("city"));
+        Assert.assertEquals("Netherlands", locationInfo.get("country"));
+        Assert.assertEquals( "NL", locationInfo.get("iso_code"));
+        Assert.assertEquals("2611", locationInfo.get("postal"));
+
+    }
+
+}
