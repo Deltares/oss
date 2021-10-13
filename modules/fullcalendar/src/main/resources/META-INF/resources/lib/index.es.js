6349a4f7b1a5abaf7c8a0415a70e1b084afe9270
rooij_e <erik.derooij@deltares.nl>
2021-10-13 13:07:22 +0200

    MYDELTARES-251: FullCalendar not showing correct TimeZone

--- modules/fullcalendar/src/main/resources/META-INF/resources/lib/index.es.js
+++ modules/fullcalendar/src/main/resources/META-INF/resources/lib/index.es.js
@@ -5,20 +5,21 @@ import interaction from '@fullcalendar/interaction'
 import resourceTimeline from '@fullcalendar/resource-timeline'
 import resourceDayGrid from '@fullcalendar/resource-daygrid'
 import resourceTimeGrid from '@fullcalendar/resource-timegrid'
+import momentTimezonePlugin from '@fullcalendar/moment-timezone'
 
 class Calendar extends React.Component {
-
 
 	constructor(props, context) {
 		super(props, context);
 		this.state = {
 			type: 'month',
 			defaultDate: this.props.startDate,
+			defaultTimeZoneId: Intl.DateTimeFormat().resolvedOptions().timeZone,
 			eventLimit: false,
 			businessHours: true,
 			defaultView: this.props.defaultView,
 			resources: {
-				url: this.props.baseUrl + '/resources/' + this.props.siteId + '/' + this.props.eventId+ '?locale=' + this.props.locale + '&p_auth=' + this.props.p_auth,
+				url: this.props.baseUrl + '/resources/' + this.props.siteId + '/' + this.props.eventId+ '?p_auth=' + this.props.p_auth,
 				method: 'GET',
 				// data: {
 				// 	username: this.props.authUser,
@@ -27,7 +28,7 @@ class Calendar extends React.Component {
 			},
 			events: {
 				url: this.props.baseUrl + '/events/' + this.props.siteId + '/' + this.props.eventId
-					+ '?portletId=' + this.props.portletId + '&layoutUuid=' + this.props.layoutUuid + '&locale=' + this.props.locale+ '&p_auth=' + this.props.p_auth,
+					+ '?portletId=' + this.props.portletId + '&layoutUuid=' + this.props.layoutUuid + '&p_auth=' + this.props.p_auth,
 				method: 'GET',
 				// data: {
 				// 	username: this.props.authUser,
@@ -42,16 +43,15 @@ class Calendar extends React.Component {
 					<div className='calendar' id='calendar'>
 						<FullCalendar
 							schedulerLicenseKey={'GPL-My-Project-Is-Open-Source'}
-							plugins={[interaction, resourceTimeline, resourceDayGrid, resourceTimeGrid]}
+							plugins={[interaction, resourceTimeline, resourceDayGrid, resourceTimeGrid,momentTimezonePlugin ]}
 							defaultView={this.state.defaultView}
-							timeZone={'UTC'}
+							timeZone={this.state.defaultTimeZoneId}
 							eventLimit={this.state.eventLimit}
 							defaultDate={this.state.defaultDate}
 							weekends={false}
-							minTime={'08:00'}
-							maxTime={'22:00'}
+							scrollTime={'08:00:00'}
 							header={ {
-								left: 'prev,next',
+								left: 'today,prev,next',
 								center: 'title',
 								right: 'horizontalDay,horizontalWeek,verticalDay,verticalWeek'
 							}}
@@ -75,7 +75,6 @@ class Calendar extends React.Component {
 								},
 							}}
 							aspectRatio={1.4}
-							contentHeight={'auto'}
 							resourceGroupField={'building'}
 							resources={this.state.resources}
 							events={this.state.events}
@@ -94,7 +93,8 @@ class Calendar extends React.Component {
 								minute: '2-digit',
 								omitZeroMinute: true,
 								meridiem: false,
-								hour12: false
+								hour12: false,
+								timeZoneName: 'short'
 
 							}}
 						/>
@@ -102,10 +102,9 @@ class Calendar extends React.Component {
 	}
 }
 
-export default function (elementId, baseUrl, siteId, eventId, startDate, defaultView, portletId, layoutUuid, locale, p_auth) {
+export default function (elementId, baseUrl, siteId, eventId, startDate, defaultView, portletId, layoutUuid, p_auth) {
 
 	ReactDOM.render( <Calendar class="fc" baseUrl={baseUrl}
 							   siteId={siteId} eventId={eventId} startDate={startDate} defaultView={defaultView}
-							   portletId={portletId} layoutUuid={layoutUuid} locale={locale} p_auth={p_auth}/>, document.getElementById(elementId))
-
+							   portletId={portletId} layoutUuid={layoutUuid} p_auth={p_auth}/>, document.getElementById(elementId))
 }
