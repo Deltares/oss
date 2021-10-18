be8be78e495e44b3d6f607f135d13652a0849e82
rooij_e <erik.derooij@deltares.nl>
2021-10-18 15:17:31 +0200

    MYDELTARES-253: Registration Join button disabled too soon as result of timezone

--- modules/common-utils/src/main/java/nl/deltares/portal/kernel/util/comparator/RegistrationDisplayContextComparator.java
+++ modules/common-utils/src/main/java/nl/deltares/portal/kernel/util/comparator/RegistrationDisplayContextComparator.java
@@ -9,7 +9,7 @@ public class RegistrationDisplayContextComparator extends OrderByComparator<Regi
 
     @Override
     public int compare(RegistrationDisplayContext context1, RegistrationDisplayContext context2) {
-        final int i = context2.getStartDate().compareTo(context1.getStartDate());
+        final int i = Long.compare(context2.getStartDateMillis(), context1.getStartDateMillis());
         if (i != 0) return i;
         return context1.getRegistration().getTitle().compareTo(context2.getRegistration().getTitle());
     }
