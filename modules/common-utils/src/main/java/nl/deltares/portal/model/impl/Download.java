372afb74ab9a676821a0e359c72c125d58e74a15
rooij_e <erik.derooij@deltares.nl>
2022-03-14 14:23:47 +0100

    Changes for direct-downloading of content

---
+++ modules/common-utils/src/main/java/nl/deltares/portal/model/impl/Download.java
@@ -0,0 +1,108 @@
+package nl.deltares.portal.model.impl;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import nl.deltares.portal.display.context.DownloadDisplayContext;
+import nl.deltares.portal.utils.DsdParserUtils;
+import nl.deltares.portal.utils.XmlContentUtils;
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Download extends AbsDsdArticle {
+
+    private int fileId = Integer.MIN_VALUE;
+    private String fileSize = "";
+    private String filePath;
+    private String fileName;
+    private String fileType;
+    private String fileTopic;
+
+
+    enum ACTION  {direct, terms, userInfo, billingInfo, subscriptions}
+
+    private final List<ACTION> requiredActions = new ArrayList<>();
+
+    public Download(JournalArticle journalArticle, DsdParserUtils articleParserUtils, Locale locale) throws PortalException {
+        super(journalArticle, articleParserUtils, locale);
+        init();
+    }
+
+    private void init() throws PortalException {
+        try {
+            Document document = getDocument();
+            String fileId = XmlContentUtils.getDynamicContentByName(document, "FileId", true);
+            if (fileId != null) this.fileId = Integer.parseInt(fileId);
+            filePath = XmlContentUtils.getDynamicContentByName(document, "FilePath", false);
+            fileName = XmlContentUtils.getDynamicContentByName(document, "FileName", false);
+
+            String options = XmlContentUtils.getDynamicContentByName(document, "RequiredActions", false);
+            parseRequiredActions(options);
+
+            fileType = XmlContentUtils.getDynamicContentByName(document, "FileType", false);
+            String fileSize = XmlContentUtils.getDynamicContentByName(document, "FileSize", true);
+            if (fileSize != null) this.fileSize = fileSize;
+
+            fileTopic = XmlContentUtils.getDynamicContentByName(document, "Topic", false);
+        } catch (Exception e) {
+            throw new PortalException(String.format("Error parsing content for article %s: %s!", getTitle(), e.getMessage()), e);
+        }
+    }
+
+    private void parseRequiredActions(String options) {
+        options = options.trim();
+        options = options.replace('\n', ' ');
+        final String[] actionList = options.split(" ");
+        for (String action : actionList) {
+            try {
+                requiredActions.add(ACTION.valueOf(action.trim()));
+            } catch (IllegalArgumentException e) {
+                //skip
+            }
+        }
+    }
+
+    @Override
+    public String getStructureKey() {
+        return "download";
+    }
+
+    public boolean isDirectDownload(){
+        return requiredActions.contains(ACTION.direct) && fileId > 0;
+    }
+
+    public boolean isSendLink(){
+        return requiredActions.contains(ACTION.direct);
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public String getFileSize(){
+        return fileSize;
+    }
+
+    public String getFileTopic() {
+        return fileTopic;
+    }
+
+    public DownloadDisplayContext toDisplayContext(ThemeDisplay themeDisplay){
+        return new DownloadDisplayContext(this, dsdParserUtils, themeDisplay);
+    }
+}
