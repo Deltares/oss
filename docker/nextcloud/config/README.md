514e5f0c1addb6939ed72eb8c5a83b86d2372bd2
rooij_e <erik.derooij@deltares.nl>
2022-03-14 14:21:27 +0100

    Setup Docker configuration with nextcloud

---
+++ docker/nextcloud/config/README.md
@@ -0,0 +1,4 @@
+Added the following to the config.php
+
+- Set logging to debug (0)
+- Add IP of host to trusted domain in conf.php. This allows API access from Liferay pod
---
+++ docker/nextcloud/config/config.php
@@ -0,0 +1,51 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' =>
+  array (
+    0 =>
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 =>
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocvqf5vtzn7j',
+  'passwordsalt' => 'QeeyI6H81nqyJXLarLzVbsgMKwCntZ',
+  'secret' => 'PJX9X10uHq5C2JBNzoAefczkKfW7FWQCRBwb01fmv2gOaRU3',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost:8180',
+    1 => '192.168.2.8:8180'
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '23.0.2.1',
+  'overwrite.cli.url' => 'http://nextcloud:8180',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mariadb-nextcloud',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpsecure' => 'tls',
+  'mail_sendmailmode' => 'smtp',
+  'mail_from_address' => 'mydeltares',
+  'mail_domain' => 'deltares.nl',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpauth' => 1,
+  'mail_smtphost' => 'smtp.office365.com',
+  'mail_smtpport' => '587',
+  'mail_smtpname' => 'mydeltares@deltares.nl',
+  'mail_smtppassword' => 'qywplkxggxrtlhhz',
+);
