ef5e3cc772377c9af1e9e87eee6dda13b7136ac7
rooij_e <erik.derooij@deltares.nl>
2022-04-25 19:27:46 +0200

    MYDELTARES-271 Implement IP validation for sanction countries

--- modules/common-utils/src/main/java/nl/deltares/portal/utils/SanctionCheckUtils.java
+++ modules/common-utils/src/main/java/nl/deltares/portal/utils/SanctionCheckUtils.java
@@ -1,9 +1,17 @@
 package nl.deltares.portal.utils;
 
 
+import java.util.Map;
+
 public interface SanctionCheckUtils {
 
-    boolean isSanctionCountry(String ipAddress);
+    /**
+     * Check if the download portal has been configured in portal-ext.properties
+     * @return True if configuration is present. Else false.
+     */
+    boolean isActive();
 
+    boolean isSanctioned(String isoCountryCode);
 
+    Map<String, String> getClientIpInfo();
 }
