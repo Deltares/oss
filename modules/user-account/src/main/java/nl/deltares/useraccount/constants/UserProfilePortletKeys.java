773ca89e9f2a51e6e11cca17151a906fa3ba41f4
rooij_e <erik.derooij@deltares.nl>
2022-08-15 16:13:12 +0200

    MYDELTARES-287: Create User Profile Pages

---
+++ modules/user-account/src/main/java/nl/deltares/useraccount/constants/UserProfilePortletKeys.java
@@ -0,0 +1,11 @@
+package nl.deltares.useraccount.constants;
+
+/**
+ * @author rooij_e
+ */
+public class UserProfilePortletKeys {
+
+	public static final String USERPROFILE =
+		"nl_deltares_useraccount_UserProfilePortlet";
+
+}
