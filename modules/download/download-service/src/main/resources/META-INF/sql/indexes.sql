8171eeac9e9367a86e9da82c4293a77a56712e07
erik de rooij <erik.derooij@deltares.nl>
2022-09-14 11:48:12 +0200

    Liferay 7.4 update Liferay module download-service

--- modules/download/download-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/download/download-service/src/main/resources/META-INF/sql/indexes.sql
@@ -5,4 +5,3 @@ create index IX_A1413B88 on Downloads_Download (groupId, userId, shareId);
 create index IX_4E718C9C on Downloads_Download (userId);
 
 create unique index IX_E94513D0 on Downloads_DownloadCount (downloadId);
-create unique index IX_C6FD00E8 on Downloads_DownloadCount (groupId, downloadId);
