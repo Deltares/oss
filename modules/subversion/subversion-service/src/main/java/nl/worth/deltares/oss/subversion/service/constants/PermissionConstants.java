4cf2aa239e8673e5b9f17ae9117b45b1623d1b13
erik de rooij <erik.derooij@deltares.nl>
2022-09-20 14:53:41 +0200

    Initial updates for Liferay 7.4 upgrade - un versioned files

---
+++ modules/subversion/subversion-service/src/main/java/nl/worth/deltares/oss/subversion/service/constants/PermissionConstants.java
@@ -0,0 +1,18 @@
+package nl.worth.deltares.oss.subversion.service.constants;
+
+
+public class PermissionConstants {
+
+  public static final String PERMISSION_READ = "r";
+  public static final String PERMISSION_WRITE = "w";
+  public static final String PERMISSION_NONE = "-";
+  public static final String[] PERMISSIONS = {
+      PERMISSION_READ, PERMISSION_WRITE, PERMISSION_NONE
+  };
+
+  public static final String TYPE_USER = "u";
+  public static final String TYPE_ROLE = "r";
+  public static final String[] TYPES = {
+      TYPE_USER, TYPE_ROLE
+  };
+}
