cc877bb7f337e57d6a2342b7867373978f4badde
erik de rooij <erik.derooij@deltares.nl>
2023-02-07 16:20:07 +0100

    MYDELTARES-340: Move User management functionality to seperate Admin portlet

--- modules/oss-forms/src/main/java/nl/deltares/forms/portlet/UserManagmentAdminFormPortlet.java
+++ modules/oss-forms/src/main/java/nl/deltares/forms/portlet/UserManagmentAdminFormPortlet.java
@@ -2,11 +2,11 @@ package nl.deltares.forms.portlet;
 
 import com.liferay.message.boards.model.MBBan;
 import com.liferay.message.boards.service.MBBanLocalServiceUtil;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.model.VirtualHost;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.VirtualHostLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadRequest;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -17,20 +17,22 @@ import nl.deltares.portal.utils.AdminUtils;
 import nl.deltares.tasks.DataRequest;
 import nl.deltares.tasks.DataRequestManager;
 import nl.deltares.tasks.impl.DeleteBannedUsersRequest;
-import nl.deltares.tasks.impl.DeleteDisabledUsersRequest;
-import nl.deltares.tasks.impl.DownloadDisabledUsersRequest;
+import nl.deltares.tasks.impl.DeleteUsersRequest;
+import nl.deltares.tasks.impl.DownloadInvalidUsersRequest;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import javax.portlet.*;
+import javax.portlet.Portlet;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author rooij_e
@@ -40,31 +42,22 @@ import java.util.concurrent.TimeUnit;
         property = {
                 "com.liferay.portlet.display-category=OSS",
                 "com.liferay.portlet.header-portlet-css=/css/main.css",
-                "com.liferay.portlet.header-portlet-javascript=/lib/oss-admin.js",
+                "com.liferay.portlet.header-portlet-javascript=/lib/user-admin.js",
                 "com.liferay.portlet.header-portlet-javascript=/lib/common.js",
                 "com.liferay.portlet.instanceable=true",
-                "javax.portlet.display-name=OSS Admin Form",
-                "javax.portlet.init-param.config-template=/admin/configuration/oss_configuration.jsp",
+                "javax.portlet.display-name=User management Admin Form",
                 "javax.portlet.init-param.template-path=/",
-                "javax.portlet.init-param.view-template=/admin/oss_admin.jsp",
+                "javax.portlet.init-param.view-template=/admin/user_admin.jsp",
                 "javax.portlet.name=" + OssConstants.OSS_ADMIN_FORM,
                 "javax.portlet.resource-bundle=content.Language",
                 "javax.portlet.security-role-ref=administrator"
         },
         service = Portlet.class
 )
-public class OssAdminFormPortlet extends MVCPortlet {
+public class UserManagmentAdminFormPortlet extends MVCPortlet {
 
     @Reference
     AdminUtils adminUtils;
-
-    @Override
-    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-
-        renderRequest.setAttribute(ConfigurationProvider.class.getName(), _configurationProvider);
-
-        super.render(renderRequest, renderResponse);
-    }
 
     @Override
     public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws IOException {
@@ -77,19 +70,16 @@ public class OssAdminFormPortlet extends MVCPortlet {
             return;
         }
         String action = ParamUtil.getString(resourceRequest, "action");
-        long siteId = ParamUtil.getLong(resourceRequest, "siteId");
-        long disabledTimeAfter = ParamUtil.getLong(resourceRequest, "disabledTime", System.currentTimeMillis() - TimeUnit.DAYS.toMillis(365));
-        if (siteId == 0) siteId = themeDisplay.getScopeGroupId();
-
-        String id = DataRequest.class.getName() + siteId + themeDisplay.getUserId();
+        String id = DataRequest.class.getName().concat("_").concat(String.valueOf(themeDisplay.getCompanyId()))
+                .concat("_").concat(String.valueOf(themeDisplay.getSiteGroupId())).concat(String.valueOf(themeDisplay.getUserId()));
         if ("deleteBannedUsers".equals(action)) {
-            deleteBannedUsersAction(id, resourceResponse, themeDisplay, siteId);
+            deleteBannedUsersAction(id, resourceResponse, themeDisplay);
         } else if ("updateStatus".equals(action)) {
             DataRequestManager.getInstance().updateStatus(id, resourceResponse);
         } else if ("downloadLog".equals(action)) {
             DataRequestManager.getInstance().downloadDataFile(id, resourceResponse);
-        } else if ("downloadDisabledUsers".equals(action)) {
-            downloadDisabledUsersAction(id, disabledTimeAfter, resourceResponse, themeDisplay);
+        } else if ("downloadInvalidUsers".equals(action)) {
+            downloadInvalidUsersAction(id, resourceResponse, themeDisplay);
         } else if ("deleteUsers".equals(action)) {
             deleteUsersAction(id, resourceRequest, resourceResponse, themeDisplay);
         } else {
@@ -108,7 +98,7 @@ public class OssAdminFormPortlet extends MVCPortlet {
                 PrintWriter writer = resourceResponse.getWriter();
                 resourceResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
                 resourceResponse.setContentType("application/json");
-                writer.print(String.format("{\"status\"' : \"error\", \"message\" : \"Failed to copy tempFile %s: %s\"}",tempFile.getName(), e.getMessage()) );
+                writer.print(String.format("{\"status\"' : \"error\", \"message\" : \"Failed to copy tempFile %s: %s\"}", tempFile.getName(), e.getMessage()));
                 return;
             }
 
@@ -116,7 +106,7 @@ public class OssAdminFormPortlet extends MVCPortlet {
             DataRequestManager instance = DataRequestManager.getInstance();
             DataRequest dataRequest = instance.getDataRequest(dataRequestId);
             if (dataRequest == null) {
-                dataRequest = new DeleteDisabledUsersRequest(dataRequestId, themeDisplay.getUserId(), themeDisplay.getCompanyId(), usersFilePath, adminUtils);
+                dataRequest = new DeleteUsersRequest(dataRequestId, themeDisplay.getUserId(), usersFilePath, adminUtils);
                 instance.addToQueue(dataRequest);
             } else if (dataRequest.getStatus() == DataRequest.STATUS.terminated || dataRequest.getStatus() == DataRequest.STATUS.nodata) {
                 instance.removeDataRequest(dataRequest);
@@ -139,20 +129,20 @@ public class OssAdminFormPortlet extends MVCPortlet {
 
 
         final File dest = new File(tempFile.getParentFile(), dataRequestId + fileName);
-        if (dest.exists()){
+        if (dest.exists()) {
             Files.delete(dest.toPath());
         }
         final Path usersFile = Files.copy(tempFile.toPath(), dest.toPath());
         return usersFile.toFile().getAbsolutePath();
     }
 
-    private void downloadDisabledUsersAction(String dataRequestId, long disabledTimeAfter, ResourceResponse resourceResponse, ThemeDisplay themeDisplay) throws IOException {
+    private void downloadInvalidUsersAction(String dataRequestId, ResourceResponse resourceResponse, ThemeDisplay themeDisplay) throws IOException {
 
         resourceResponse.setContentType("application/json");
         DataRequestManager instance = DataRequestManager.getInstance();
         DataRequest dataRequest = instance.getDataRequest(dataRequestId);
         if (dataRequest == null) {
-            dataRequest = new DownloadDisabledUsersRequest(dataRequestId, disabledTimeAfter, themeDisplay.getUserId(), adminUtils);
+            dataRequest = new DownloadInvalidUsersRequest(dataRequestId, themeDisplay.getUserId(), adminUtils);
             instance.addToQueue(dataRequest);
         } else if (dataRequest.getStatus() == DataRequest.STATUS.terminated || dataRequest.getStatus() == DataRequest.STATUS.nodata) {
             instance.removeDataRequest(dataRequest);
@@ -164,30 +154,22 @@ public class OssAdminFormPortlet extends MVCPortlet {
         writer.println(statusMessage);
     }
 
-    private void deleteBannedUsersAction(String dataRequestId, ResourceResponse resourceResponse, ThemeDisplay themeDisplay, long siteId) throws IOException {
-        List<MBBan> bannedUsers = MBBanLocalServiceUtil.getBans(siteId, 0, 100);
-
+    private void deleteBannedUsersAction(String dataRequestId, ResourceResponse resourceResponse, ThemeDisplay themeDisplay) throws IOException {
+        List<MBBan> bannedUsers = getBannedUsersAllSites();
         if (bannedUsers.size() == 0) {
             PrintWriter writer = resourceResponse.getWriter();
             resourceResponse.setContentType("text/plain");
-            try {
-                resourceResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
-                Group group = GroupLocalServiceUtil.getGroup(siteId);
-                writer.printf("No banned users found for site %s (%d)", group.getName(), siteId);
-                return;
-            } catch (PortalException e) {
-                resourceResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                writer.printf("Error getting scope group for siteId %d: %s", siteId, e.getMessage());
-                return;
-            }
+            resourceResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            writer.println("No banned users found for any site.");
+            return;
         }
         resourceResponse.setContentType("application/json");
         DataRequestManager instance = DataRequestManager.getInstance();
         DataRequest dataRequest = instance.getDataRequest(dataRequestId);
         if (dataRequest == null) {
-            dataRequest = new DeleteBannedUsersRequest(dataRequestId, siteId, themeDisplay.getUserId(), bannedUsers, adminUtils);
+            dataRequest = new DeleteBannedUsersRequest(dataRequestId, themeDisplay.getUserId(), bannedUsers, adminUtils);
             instance.addToQueue(dataRequest);
-        } else if (dataRequest.getStatus() == DataRequest.STATUS.terminated || dataRequest.getStatus() == DataRequest.STATUS.nodata ) {
+        } else if (dataRequest.getStatus() == DataRequest.STATUS.terminated || dataRequest.getStatus() == DataRequest.STATUS.nodata) {
             instance.removeDataRequest(dataRequest);
         }
         resourceResponse.setStatus(HttpServletResponse.SC_OK);
@@ -197,12 +179,17 @@ public class OssAdminFormPortlet extends MVCPortlet {
         writer.println(statusMessage);
     }
 
+    private List<MBBan> getBannedUsersAllSites() {
 
-    private ConfigurationProvider _configurationProvider;
-
-    @Reference
-    protected void setConfigurationProvider(ConfigurationProvider configurationProvider) {
-        _configurationProvider = configurationProvider;
+        List<MBBan> bans = new ArrayList<>();
+        final List<VirtualHost> virtualHosts = VirtualHostLocalServiceUtil.getVirtualHosts(0, 10);
+        for (VirtualHost virtualHost : virtualHosts) {
+            final long companyId = virtualHost.getCompanyId();
+            for (Group companyGroup : GroupLocalServiceUtil.getCompanyGroups(companyId, 0, 50)) {
+                if (!companyGroup.isSite()) continue;
+                bans.addAll(MBBanLocalServiceUtil.getBans(companyGroup.getGroupId(), 0, 100));
+            }
+        }
+        return bans;
     }
-
 }
