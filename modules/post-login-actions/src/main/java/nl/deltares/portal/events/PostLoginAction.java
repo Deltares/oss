f8b993505ef9e361b128034e46c75dd956d668d4
erik de rooij <erik.derooij@deltares.nl>
2023-02-14 19:30:16 +0100

    Revert "TEMP Checkin for testing only!!!"
    This reverts commit 445cb9798776f5dac530c02162ee7f61fe395036.

--- modules/post-login-actions/src/main/java/nl/deltares/portal/events/PostLoginAction.java
+++ modules/post-login-actions/src/main/java/nl/deltares/portal/events/PostLoginAction.java
@@ -1,6 +1,5 @@
 package nl.deltares.portal.events;
 
-import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManagerUtil;
 import com.liferay.portal.kernel.events.LifecycleAction;
 import com.liferay.portal.kernel.events.LifecycleEvent;
 import com.liferay.portal.kernel.log.Log;
@@ -78,6 +77,7 @@ public class PostLoginAction implements LifecycleAction {
 		}
 
 		if (keycloakUtils.isActive()){
+
 			try {
 
 				byte[] bytes = keycloakUtils.getUserAvatar(user.getEmailAddress());
@@ -106,17 +106,10 @@ public class PostLoginAction implements LifecycleAction {
 		}
 
 		if (downloadUtils.isActive()){
-			LOG.info("Start waiting");
-			try {
-				Thread.currentThread().join(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			LOG.info("Finished waiting");
-//			final LayoutSet layoutSet = (LayoutSet) request.getAttribute(WebKeys.VIRTUAL_HOST_LAYOUT_SET);
-//			//Check if users has any pending shares that need to be updated
-//			downloadUtils.updatePendingShares(user, layoutSet.getGroupId());
-//			downloadUtils.updateProcessingShares(user, layoutSet.getGroupId());
+			final LayoutSet layoutSet = (LayoutSet) request.getAttribute(WebKeys.VIRTUAL_HOST_LAYOUT_SET);
+			//Check if users has any pending shares that need to be updated
+			downloadUtils.updatePendingShares(user, layoutSet.getGroupId());
+			downloadUtils.updateProcessingShares(user, layoutSet.getGroupId());
 		}
 
 		if (geoIpUtils != null){
