7cce90cdb96a1ae02de839ace76a403295f15a78
erik de rooij <erik.derooij@deltares.nl>
2023-03-08 17:20:11 +0100

    MYDELTARES-337: Automatically generate license files from webportal

--- modules/download/download-service/src/main/java/nl/deltares/oss/download/upgrade/DownloadServiceUpgrade.java
+++ modules/download/download-service/src/main/java/nl/deltares/oss/download/upgrade/DownloadServiceUpgrade.java
@@ -1,12 +1,15 @@
 package nl.deltares.oss.download.upgrade;
 
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import nl.deltares.oss.download.upgrade.v1_1_0.DownloadTableAddColumnLicenseDownloadUrl;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = UpgradeStepRegistrator.class)
 public class DownloadServiceUpgrade implements UpgradeStepRegistrator {
     @Override
     public void register(Registry registry) {
-//nothing to update yet
+        registry.register(
+                "1.0.0", "1.1.0",
+                new DownloadTableAddColumnLicenseDownloadUrl());
     }
 }
