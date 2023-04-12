a83adf66db756e3aae01642f74c354bd533d84c2
erik de rooij <erik.derooij@deltares.nl>
2023-04-12 11:30:27 +0200

    MYDELTARES-359: Enable SendInBlue subscriptions

--- modules/common-utils/src/main/java/nl/deltares/portal/utils/EmailSubscriptionUtils.java
+++ modules/common-utils/src/main/java/nl/deltares/portal/utils/EmailSubscriptionUtils.java
@@ -1,15 +1,25 @@
 package nl.deltares.portal.utils;
 
 import com.liferay.portal.kernel.model.User;
-import nl.deltares.portal.model.impl.Subscription;
+import nl.deltares.portal.model.subscriptions.Subscription;
 
 import java.util.List;
 
 public interface EmailSubscriptionUtils {
 
     boolean isActive();
-    boolean isSubscribed(User user, List<Subscription> subscriptions) throws Exception;
-    boolean isSubscribed(User user, Subscription subscription) throws Exception;
-    void subscribe(User user, Subscription subscription) throws Exception;
-    void unsubscribe(User user, Subscription subscription) throws Exception;
+
+    boolean isSubscribed(String email, List<String> subscriptionIds) throws Exception;
+
+    boolean isSubscribed(String email, String subscriptionId) throws Exception;
+
+    void subscribe(User user, String subscriptionId) throws Exception;
+
+    void unsubscribe(String email, String subscriptionId) throws Exception;
+
+    void deleteUser(String email) throws Exception;
+
+    List<Subscription> getSubscriptions(String emailAddress) throws Exception;
+
+
 }
