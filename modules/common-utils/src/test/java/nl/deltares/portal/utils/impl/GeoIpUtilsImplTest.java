2c74996ec762ef0f053790cd13ecb4825288b12d
erik de rooij <erik.derooij@deltares.nl>
2023-06-14 15:54:02 +0200

    Updated GeoLite2-City.mmdb database for better location accuracy

--- modules/common-utils/src/test/java/nl/deltares/portal/utils/impl/GeoIpUtilsImplTest.java
+++ modules/common-utils/src/test/java/nl/deltares/portal/utils/impl/GeoIpUtilsImplTest.java
@@ -1,12 +1,17 @@
 package nl.deltares.portal.utils.impl;
 
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CountryResponse;
 import nl.deltares.mock.MockProps;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Map;
 import java.util.Properties;
 
@@ -27,6 +32,19 @@ public class GeoIpUtilsImplTest {
 
     }
 
+    @Test
+    public void testGeoLiteDb() throws IOException, GeoIp2Exception {
+
+        File database = new File(getClass().getResource("/geoip/GeoLite2-City.mmdb").getFile());
+        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+
+        //Switzerland
+        final InetAddress byName = InetAddress.getByName("212.102.36.251");
+        final CountryResponse country = reader.country(byName);
+
+
+        Assert.assertEquals("CH", country.getCountry().getIsoCode());
+    }
     @Test
     public void testLocationInfo() {
 
