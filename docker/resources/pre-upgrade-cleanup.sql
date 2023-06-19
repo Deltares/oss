3ec09c3d7d34d2b433ce8fcec25b49f44e0ba251
erik de rooij <erik.derooij@deltares.nl>
2023-06-19 08:10:18 +0200

    Updated cleanup script 7.1 -> 7.4

--- docker/resources/pre-upgrade-cleanup.sql
+++ docker/resources/pre-upgrade-cleanup.sql
@@ -20,21 +20,48 @@ DELETE FROM CalendarBooking;
 DELETE FROM CalendarResource;
 DELETE FROM ChangesetCollection;
 DELETE FROM EmailAddress;
+DELETE FROM DLFileVersionPreview;
 DELETE FROM SystemEvent;
 DELETE FROM OAuth2Application;
 DELETE FROM OAuth2Authorization;
+DELETE FROM Phone;
 DELETE FROM SamlSpAuthRequest;
 DELETE FROM SamlSpSession;
+DELETE FROM SocialActivity;
+DELETE FROM SocialActivityAchievement;
+DELETE FROM SocialActivityCounter;
+DELETE FROM SocialActivityLimit;
+DELETE FROM SocialActivitySet;
+DELETE FROM SocialActivitySetting;
 DELETE FROM SystemEvent;
 DELETE FROM SVN_SVNLog;
 DELETE FROM Subversion_RepositoryLog;
+DELETE FROM Ticket;
 DELETE FROM TrashEntry;
 DELETE FROM TrashVersion;
 DELETE FROM Twitter_Feed;
+DELETE FROM UserNotificationDelivery;
+DELETE FROM UserNotificationEvent;
 DELETE FROM Website;
+DELETE FROM WikiNode;
 DELETE FROM WikiPage;
 DELETE FROM WikiPageResource;
 DELETE FROM WorkflowDefinitionLink ;
+
+DROP TABLE deltares_Repository;
+DROP TABLE deltares_RepositoryFolder;
+DROP TABLE deltares_RepositoryFolderPermission;
+DROP TABLE deltares_RepositoryFolderUserPermission;
+DROP TABLE deltares_RepositoryLog;
+
+DROP TABLE Subversion_Repository;
+DROP TABLE Subversion_RepositoryFolder;
+DROP TABLE Subversion_RepositoryFolderPermission;
+DROP TABLE Subversion_RepositoryLog;
+DROP TABLE subversion_SVNLog;
+
+-- Clean configurations
+DELETE FROM Configuration_;
 
 --
 -- Remove orphaned structures, templates, layouts
@@ -57,9 +84,6 @@ DELETE FROM LayoutFriendlyURL WHERE groupId NOT IN (SELECT g.groupId FROM Group_
 DELETE FROM Layout WHERE groupId NOT IN (SELECT g.groupId FROM Group_ g );
 DELETE FROM LayoutSet WHERE groupId NOT IN (SELECT g.groupId FROM Group_ g );
 
--- Clean configurations
-DELETE FROM Configuration_;
-
 --
 -- Following steps are POST-Upgrade SQL statements.
 -- These can be executed after upgrade to clean up old records.
@@ -68,8 +92,8 @@ DELETE FROM Configuration_;
 --
 -- Cleanup orphaned user records
 --
-DELETE FROM UserNotificationEvent WHERE userId NOT IN (SELECT u.userId FROM User_ u)
-DELETE FROM MBMessage  WHERE userId NOT IN (SELECT u.userId FROM User_ u) AND createDate < '2018-01-01'
+DELETE FROM UserNotificationEvent WHERE userId NOT IN (SELECT u.userId FROM User_ u);
+DELETE FROM MBMessage  WHERE userId NOT IN (SELECT u.userId FROM User_ u) AND createDate < '2018-01-01';
 -- We can only remove users from the User_ table when we are sure no other tables link to these users
 -- We can only remove user group records from Group_ when we are sure the corresponding users no longer exist.
 
