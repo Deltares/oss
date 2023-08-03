c91b3ff406ccabfd8f9e3880b5150a676749e6a8
erik de rooij <erik.derooij@deltares.nl>
2023-08-03 09:16:04 +0200

    MYDELTARES-378: Remove sanction check configuration and replace with country management

--- modules/common-utils/src/main/java/nl/deltares/portal/utils/SanctionCheckUtils.java
+++ modules/common-utils/src/main/java/nl/deltares/portal/utils/SanctionCheckUtils.java
@@ -5,5 +5,7 @@ public interface SanctionCheckUtils {
 
     boolean isSanctionedByCountyCode(String isoCountryCode);
 
+    boolean isSanctionedByCountyCode(long companyId, String isoCountryCode);
+
     boolean isSanctionedByIp(String remoteAddress);
 }
