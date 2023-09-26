af26fbebcbe8172ce64b7b1b4aabf22b3993d582
erik de rooij <erik.derooij@deltares.nl>
2023-09-26 10:59:43 +0200

    Added Licenses page for user profile

--- modules/user-account/src/main/java/nl/deltares/useraccount/portlet/PasswordPortlet.java
+++ modules/user-account/src/main/java/nl/deltares/useraccount/portlet/PasswordPortlet.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 @Component(
         immediate = true,
         property = {
+                "javax.portlet.version=3.0",
                 "com.liferay.portlet.display-category=OSS-account",
                 "com.liferay.portlet.header-portlet-css=/css/main.css",
                 "com.liferay.portlet.instanceable=true",
