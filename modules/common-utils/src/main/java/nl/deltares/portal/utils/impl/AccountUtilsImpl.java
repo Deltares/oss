1152bc2156c78d94dfc87e987b65ce011a697513
rooij_e <erik.derooij@deltares.nl>
2025-06-30 12:56:39 +0200

    Upgrading to 2025.q2

--- modules/common-utils/src/main/java/nl/deltares/portal/utils/impl/AccountUtilsImpl.java
+++ modules/common-utils/src/main/java/nl/deltares/portal/utils/impl/AccountUtilsImpl.java
@@ -45,8 +45,9 @@ public class AccountUtilsImpl implements AccountUtils {
         final ServiceContext serviceContext = new ServiceContext();
         serviceContext.setCompanyId(user.getCompanyId());
         serviceContext.setScopeGroupId(user.getGroupId());
+        String externalReference = user.getUserId() + '_' + user.getEmailAddress();
         //we can only create personal accounts through the code
-        final AccountEntry accountEntry = _accountEntryLocalService.addAccountEntry(
+        final AccountEntry accountEntry = _accountEntryLocalService.addAccountEntry(externalReference,
                 user.getUserId(),
                 0, user.getScreenName(), user.getFullName(), null,
                 user.getEmailAddress(), new byte[0],
