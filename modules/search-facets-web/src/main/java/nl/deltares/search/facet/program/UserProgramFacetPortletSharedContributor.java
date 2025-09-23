1365cfcd38c4ed2e836adf337e578e1498931779
rooij_e <erik.derooij@deltares.nl>
2025-09-23 14:53:05 +0200

    Backport changes from branch 2025.q2.2

--- modules/search-facets-web/src/main/java/nl/deltares/search/facet/program/UserProgramFacetPortletSharedContributor.java
+++ modules/search-facets-web/src/main/java/nl/deltares/search/facet/program/UserProgramFacetPortletSharedContributor.java
@@ -37,12 +37,12 @@ public class UserProgramFacetPortletSharedContributor implements PortletSharedSe
 
             List<String> entryClassPKs;
             if (showRegistrationForOthers(portletSharedSearchSettings)){
-                entryClassPKs = _dsdSessionUtils.getUserRegistrations(user, groupId)
+                entryClassPKs = _dsdSessionUtils.getResourceIdsByAuthorAndGroup(user, groupId)
                         .stream()
                         .map(String::valueOf)
                         .collect(Collectors.toList());
             } else {
-                entryClassPKs = _dsdSessionUtils.getUserRegistrations(user, groupId)
+                entryClassPKs = _dsdSessionUtils.getResourceIdsByUserAndGroup(user, groupId)
                         .stream()
                         .map(String::valueOf)
                         .collect(Collectors.toList());
